205bb58789880ad6f04de726933e35d705e899c0
Gulshan Kumar <gulshan19112005@gmail.com>
2025-12-17 13:14:03 +0530

    Update MileStone1ProssessingDays.py code

--- MileStone1ProssessingDays.py
+++ MileStone1ProssessingDays.py
@@ -1,15 +1,11 @@
-# ===============================
 # IMPORT PACKAGES
-# ===============================
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
 pd.set_option("display.max_columns", None)
 
-# ===============================
 # PART 1: LARGE VISA DATASET
-# ===============================
 print("\n===== PART 1: LARGE VISA DATASET =====\n")
 
 data = {
@@ -40,33 +36,26 @@ data = {
 df = pd.DataFrame(data)
 print("Original DataFrame:\n", df)
 
-# ===============================
+
 # DATE CONVERSION
-# ===============================
 df["application_date"] = pd.to_datetime(df["application_date"])
 df["decision_date"] = pd.to_datetime(df["decision_date"])
 
 print("\nAfter Date Conversion:\n", df)
 
-# ===============================
 # PROCESSING DAYS
-# ===============================
 df["processing_days"] = (
     df["decision_date"] - df["application_date"]
 ).dt.days
 
 print("\nAfter Calculating Processing Days:\n", df)
 
-# ===============================
 # ENCODING
-# ===============================
 df_encoded = pd.get_dummies(df, columns=["country", "visa_type"])
 
 print("\nEncoded DataFrame:\n", df_encoded)
 
-# ===============================
 # MACHINE LEARNING
-# ===============================
 X = df_encoded.drop(
     columns=["processing_days", "application_date", "decision_date"]
 )
@@ -75,9 +64,8 @@ y = df_encoded["processing_days"]
 model = LinearRegression()
 model.fit(X, y)
 
-# ===============================
+
 # PREDICTION SAMPLE
-# ===============================
 sample_input = pd.DataFrame(
     np.zeros((1, len(X.columns))),
     columns=X.columns
